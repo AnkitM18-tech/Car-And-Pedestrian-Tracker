bf15ba28af2df759ff241b44fcd7306315ce3b69
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-07-25 01:17:15 +0530

    Image and Classifier Loaded

--- Car_And_Pedestrain_Tracker.py
+++ Car_And_Pedestrain_Tracker.py
@@ -1,3 +1,21 @@
 import cv2
 
+# Image path
+imageFile = "sources/car-image.jpg"
+
+# Pre-Trained car-image Classifier
+classifier_file = "car_detector.xml"
+
+# Create OpenCV image
+img = cv2.imread(imageFile)
+
+#Create car-classififer
+car_tracker = cv2.CascadeClassifier(classifier_file)
+
+# display the image with faces detected
+cv2.imshow("Car and Pedestrian detector",img)
+
+# Don't autoclose and wait (here for a key press)
+cv2.waitKey()
+
 print("Code Completed")
