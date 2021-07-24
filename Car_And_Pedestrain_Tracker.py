4d4e4fc6523710b0b3e9224aa14912792c3f64b7
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-07-25 02:20:24 +0530

    Detector Code Completed

--- Car_And_Pedestrain_Tracker.py
+++ Car_And_Pedestrain_Tracker.py
@@ -9,7 +9,7 @@ imageFile = "sources/car-image.jpg"
 videoFile = "sources/cars and pedestrians.mp4"
 
 # Pre-Trained car-image Classifier
-classifier_file = "car_detector.xml"
+car_classifier_file = "car_detector.xml"
 
 #Pre-Trained Fullbody classifier
 pedestrian_classifier_file = "fullbody.xml"
@@ -23,7 +23,7 @@ img = cv2.imread(imageFile)
 video = cv2.VideoCapture(videoFile)
 
 #Create car-classififer
-car_tracker = cv2.CascadeClassifier(classifier_file)
+car_tracker = cv2.CascadeClassifier(car_classifier_file)
 
 #Create pedestrian classifier
 pedestrian_tracker = cv2.CascadeClassifier(pedestrian_classifier_file)
@@ -39,29 +39,39 @@ while True:
     else:
         break
 
-'''
-#Convert to grayscale (needed for haar cascade)
-black_and_white_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-'''
+    '''
+    #Convert to grayscale (needed for haar cascade)
+    black_and_white_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    '''
 
-#Detect Cars
-cars = car_tracker.detectMultiScale(grayscale_frame)
+    #Detect Cars
+    cars = car_tracker.detectMultiScale(grayscale_frame)
 
-#Detect Pedestrians
-pedestrians = pedestrian_tracker.detectMultiScale(grayscale_frame)
+    #Detect Pedestrians
+    pedestrians = pedestrian_tracker.detectMultiScale(grayscale_frame)
 
-#Draw Rectangles around the car
-for (x,y,w,h) in cars:
-    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    #Draw Rectangles around the car
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-# Draw Rectangles around the Pedestrians
+    # Draw Rectangles around the Pedestrians
+    for (x,y,w,h) in pedestrians:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
 
 
+    # display the frame with cars and pedestrian detected
+    cv2.imshow("Car and Pedestrian detector",frame)
 
-# display the frame with cars and pedestrian detected
-cv2.imshow("Car and Pedestrian detector",frame)
+    # Don't autoclose and wait (here for a key press)
+    key = cv2.waitKey(1)
 
-# Don't autoclose and wait (here for a key press)
-cv2.waitKey(1)
+    #Stop if Q is pressed
+    if key == 81 or key == 113:
+        break
+
+#Release the VideoCapture Object
+video.release()
 
 print("Code Completed")
+
+# ! The Commented Section is for still image detection
